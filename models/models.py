6218f0fee04130b93683b282851adb61b80d9031
obopodo <bpodolnyi@gmail.com>
2022-12-12 00:32:16 +0200

    upd: fix issues

--- models/models.py
+++ models/models.py
@@ -10,19 +10,20 @@ from rectools.models.base import ModelBase
 Scores = Union[Sequence[float], np.ndarray]
 
 class UsersCoverageModel(ModelBase):
-    def __init__(self, users_percentage: float) -> None:
+    def __init__(self, users_percentage: float, k_recom: int = 10) -> None:
         super().__init__()
         self.users_percentage = users_percentage
+        self.k_recom = k_recom
         
 
     def _get_top_items_covered_users(self, matrix: csr_matrix):
         assert matrix.format == 'csr'
-        n_users = int(matrix.shape[0] * self.users_percentage / 100)
+        n_users = int(matrix.shape[0] * self.users_percentage)
         print(f'n_users: {n_users} ({100 * n_users / matrix.shape[0] :.2f}%)')
         
         item_set = []
         covered_users = np.zeros(matrix.shape[0], dtype=bool) # true if a user has been checked already
-        while (covered_users.sum() < n_users): # stop if the number of checked users exceeds the limit
+        while (covered_users.sum() < n_users) or (len(item_set) < self.k_recom): # stop if the number of checked users exceeds the limit
             top_item = mode(matrix[~covered_users].indices)[0][0] # most frequent item among yet unchecked users 
             item_set.append(top_item)
             covered_users += np.maximum.reduceat(matrix.indices==top_item, matrix.indptr[:-1], dtype=bool) 
@@ -31,28 +32,9 @@ class UsersCoverageModel(ModelBase):
         
     def _fit(self, dataset: Dataset):
         matrix = dataset.get_user_item_matrix()
-        item_set, covered_users = self._get_top_items_covered_users(matrix)
-        self.pop_covered = dataset.item_id_map.convert_to_external(item_set) 
+        self.pop_covered, covered_users = self._get_top_items_covered_users(matrix)
         return self
     
-
-    # def recommend(
-    #     self, 
-    #     users: Union[Sequence[Hashable], np.ndarray],
-    #     dataset: Dataset, # оставлено для обратной совместимости
-    #     k: int,
-    #     filter_viewed=True,
-    #     **kwargs
-    # ) -> pd.DataFrame:
-    #     recom = pd.DataFrame()
-    #     for u in users:
-    #         user_recom = pd.DataFrame({
-    #             'user_id': [u]*k, 
-    #             'item_id': self.pop_covered[:k], 
-    #             'rank': np.arange(1, k+1)
-    #         })
-    #         recom = pd.concat((recom, user_recom))
-    #     return recom
 
     def _recommend_u2i(
         self,
@@ -60,16 +42,17 @@ class UsersCoverageModel(ModelBase):
         dataset: Dataset,
         k: int,
         filter_viewed: bool,
-        # sorted_item_ids_to_recommend: Optional[np.ndarray],
+        sorted_item_ids_to_recommend: Optional[np.ndarray],
         **kwargs
     ) -> Tuple[InternalIds, InternalIds, Scores]:
 
-        recom = pd.DataFrame()
+        # recom = pd.DataFrame()
+        reco_user_ids = []
+        reco_item_ids= []
+        reco_scores = []
+        
         for u in user_ids:
-            user_recom = pd.DataFrame({
-                'user_id': [u]*k, 
-                'item_id': self.pop_covered[:k], 
-                'rank': np.arange(1, k+1)
-            })
-            recom = pd.concat((recom, user_recom))
-        return recom
+            reco_user_ids.extend([u]*k)
+            reco_item_ids.extend(list(self.pop_covered[:k]))
+            reco_scores.extend(list(np.arange(k, 0, -1)))
+        return reco_user_ids, reco_item_ids, reco_scores 
